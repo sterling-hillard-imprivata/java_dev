442bfaca345875a0250b84f957a28c58a3c725fb
Sterling Hillard <shillard@Sterlings-Air.MG8702>
2022-06-08 08:45:12 -0500

    chapter 6

---
+++ com/craftinginterpreters/lox/Expr.java
@@ -0,0 +1,36 @@
+package com.craftinginterpreters.lox;
+
+abstract class Expr {
+  interface Visitor<R> {
+    R visitGroupingExpr(Grouping expr);
+  }
+
+  static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+  static class Grouping extends Expr {
+    Grouping(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
+    final Expr expression;
+    }
+
+  static
+  }
+
+  // Other expressions...
+}
